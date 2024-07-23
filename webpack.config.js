b0a88589cb35e4c06374fd54a07357572a08ee5d
Danilo Salvador <danilo.salvador@smartlogic.com.br>
2024-07-23 12:29:21 -0300

    Carregando o CSS

--- webpack.config.js
+++ webpack.config.js
@@ -26,4 +26,12 @@ module.exports = {
             favicon: path.resolve(__dirname, "src", "assets", "scissors.svg"),
         }),
     ],
+    module : {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    }
 }
