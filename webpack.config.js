02386191ec101761baacaa1bf01a5399371b61f8
Danilo Salvador <danilo.salvador@smartlogic.com.br>
2024-07-23 12:47:08 -0300

    Incluindo o Babel na aplicação

--- webpack.config.js
+++ webpack.config.js
@@ -39,8 +39,19 @@ module.exports = {
         rules: [
             {
                 test: /\.css$/i,
+                exclude: "/node_modules",
                 use: ["style-loader", "css-loader"]
-            }
-        ]
-    }
+            },
+            {
+                test: /\.js$/i,
+                exclude: "/node_modules",
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"],
+                    },
+                },
+            },
+        ],
+    },
 }
