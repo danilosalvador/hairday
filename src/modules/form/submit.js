18ad0d843b76b8025bfad081241e997d2ebb3c0b
Danilo Salvador <danilo.salvador@smartlogic.com.br>
2024-07-24 20:16:19 -0300

    Enviando um horário selecionado para a API

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -2,6 +2,7 @@ import dayjs from "dayjs"
 
 const form = document.querySelector("form")
 const selectedDate = document.getElementById("date")
+const name = document.getElementById("client")
 
 const todayDate = dayjs(new Date()).format("YYYY-MM-DD")
 
@@ -11,4 +12,26 @@ selectedDate.min = todayDate
 form.onsubmit = (event) => {
     event.preventDefault()
 
+    try {
+        if (!name) {
+            alert("Informe o nome do cliente")
+            return
+        }
+
+        const hourSelected = document.querySelector(".hour-selected")
+
+        if (!hourSelected) {
+            alert("Selecione um horário!")
+            return
+        }
+
+        const [hour, _] = hourSelected.innerText.split(":")
+        
+        const when = dayjs(selectedDate.value).add(hour, "hour")
+        const id = new Date().getTime()
+
+    } catch (error) {
+        alert("Não foi possível realizar o agendamento")
+        console.log(error)
+    }
 }
