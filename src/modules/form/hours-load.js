e1c9e14f2733411d29a29f7ca3a07feb4decce42
Danilo Salvador <danilo.salvador@smartlogic.com.br>
2024-07-24 20:01:12 -0300

    Separando os horários pelo período

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -2,6 +2,8 @@ import dayjs from "dayjs"
 
 import { openingHours } from "../../utils/opening-hours.js"
 
+const hours = document.getElementById("hours")
+
 export function hoursLoad({ date }) {
     const opening = openingHours.map((hour) => {
         const [ scheduleHour, _ ] = hour.split(":")
@@ -13,4 +15,32 @@ export function hoursLoad({ date }) {
             available: isHourIsPast
         }
     })
+
+    opening.forEach(({ hour, available }) => {
+        const li = document.createElement("li")
+
+        li.classList.add("hour")
+        li.classList.add(available ? "hour-available" : "hour-unavailable")
+
+        li.textContent = hour
+
+        if (hour === "9:00") {
+            hourHeader("Manhã")
+        } else if (hour === "12:00") {
+            hourHeader("Tarde")
+        } else if (hour === "18:00") {
+            hourHeader("Noite")
+        }
+
+        hours.append(li)
+    })
+}
+
+function hourHeader(title) {
+    const li = document.createElement("li")
+
+    li.classList.add("hour-period")
+    li.textContent = title
+
+    hours.append(li)
 }
