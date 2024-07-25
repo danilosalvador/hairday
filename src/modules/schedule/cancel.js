acfbbe02a820b771d3092ee2ca7f6a85efe6fa34
Danilo Salvador <danilo.salvador@smartlogic.com.br>
2024-07-25 20:23:56 -0300

    Removendo um agendamento da API

--- src/modules/schedule/cancel.js
+++ src/modules/schedule/cancel.js
@@ -1,7 +1,10 @@
+import { schedulesDay } from "./load.js"
+import { scheduleCancel } from "../../services/schedule-cancel.js"
+
 const periods = document.querySelectorAll(".period")
 
 periods.forEach((period) => {
-    period.addEventListener("click", (event) => {
+    period.addEventListener("click", async (event) => {
         if (event.target.classList.contains("cancel-icon")) {
             //Obtém o li pai do elemento clicado
             const item = event.target.closest("li")
@@ -11,7 +14,8 @@ periods.forEach((period) => {
                 const isConfirm = confirm("Tem certeza que deseja cancelar o agendamento?")
 
                 if (isConfirm) {
-                    
+                    await scheduleCancel({ id })
+                    await schedulesDay()
                 }
             }
         }
