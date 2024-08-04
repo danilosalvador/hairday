2bac4822ccd4454ba2afcfe38ce63f34df7f0665
Danilo Salvador <danilo.salvador@smartlogic.com.br>
2024-08-04 12:15:47 -0300

    Configuração para MyJsonServer

--- dist/main.js
+++ dist/main.js
@@ -179,7 +179,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   apiConfig: () => (/* binding */ apiConfig)\n/* harmony export */ });\nvar apiConfig = {\n  baseURL: \"http://localhost:3333\"\n};\n\n//# sourceURL=webpack://hairday/./src/services/api-config.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   apiConfig: () => (/* binding */ apiConfig)\n/* harmony export */ });\nvar apiConfig = {\n  // baseURL: \"http://localhost:3333\",\n  baseURL: \"https://my-json-server.typicode.com/danilosalvador/hairday\"\n};\n\n//# sourceURL=webpack://hairday/./src/services/api-config.js?");
 
 /***/ }),
 
